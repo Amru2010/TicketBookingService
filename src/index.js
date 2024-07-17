2bcf975c73cc13571bbc8c635f04fc2cc6a5d815
Amru2010 <amrutanshjha2010@gmail.com>
2024-07-17 12:50:06 +0530

    First Commit, made basic skeletal structure

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const app=express();
+
+const {PORT} = require('./config/serverConfig');
+const apiRoutes=require('./routes/index');
+const db=require('./models/index');
+
+const setupAndStartServer=()=>{
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+    app.use('/api',apiRoutes);
+
+    app.listen(PORT,()=>{
+        console.log(`Server starting at Port: ${PORT}`);
+        if(DB_SYNC){
+            db.sequelize.sync({alter:true});
+        }
+    });
+}
+
+setupAndStartServer();
