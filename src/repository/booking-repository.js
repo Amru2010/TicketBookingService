aa479067bf7911ab04e22c83900fd8a6bb1b1052
Amru2010 <amrutanshjha2010@gmail.com>
2024-07-19 23:40:09 +0530

    Completed create booking

--- src/repository/booking-repository.js
+++ src/repository/booking-repository.js
@@ -22,8 +22,22 @@ class BookingRepository{
         }
     }
 
-    async update(){
-        
+    async update(bookingId, data){
+        try {
+            const booking=await Booking.findByPk(bookingId);
+            if(data.status){
+                booking.status=data.status;
+            }
+            await booking.save();
+            return booking;
+        } catch (error) {
+            throw new AppError(
+                'RepositoryError',
+                'Cannot create Booking',
+                'There was some issue with updating the booking',
+                StatusCodes.INTERNAL_SERVER_ERROR
+            );
+        }
     }
 }
 
